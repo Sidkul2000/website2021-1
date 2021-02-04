959bbeed1427e8f4439b9cf68493ba5e7e3ca0ca
Yatish Kelkar <yatish1606@gmail.com>
2021-02-04 10:46:05 +0530

    sidebar setup

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { Box, Home, TrendingUp, Users } from 'react-feather'
 import { useTheme } from '../../context/ThemeContext'
 
 import './sidebar.css'
+
+const menuList = [
+    {
+        icon: <Home className="menu-icon"/>
+    },
+    {
+        icon: <Box className="menu-icon"/>
+    },
+    {
+        icon: <Users className="menu-icon"/>
+    },
+    {
+        icon: <TrendingUp className="menu-icon"/>
+    },
+]
 
 const Sidebar = ({
     props
@@ -12,8 +28,29 @@ const Sidebar = ({
     return (
         <div className="sidebar-container">
         
-        <button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>Change theme</button>
-        {theme}
+            <div className="app-logo-container">
+
+            </div>
+
+            <div className="menu-container">
+                {
+                    menuList.map(item => {
+                        return (
+                            <div className="menu-option">
+                                {item.icon}
+                                
+                            </div>
+                        )
+                    })
+                }
+            </div>
+
+            <div className="app-logo-container">
+                <button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>Change theme</button>
+                {theme}
+            </div>
+
+        
         </div>
     )
 }
